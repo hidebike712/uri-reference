c66ed8ad13401a05bf80b2074af97f28aaa5bf77
hidebike712 <hidebike712@users.noreply.github.com>
2025-12-13 10:10:58 +0000

    deploy: e73a0b69b721556937a715fef01673fabc3126e8

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.czeal.rfc3986"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.czeal.rfc3986"}]
